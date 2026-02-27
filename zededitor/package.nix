58a79c6cf993a87b94c7e42f34d15826462fe029
tacogips <me@tacogips.me>
2026-02-27 15:23:06 +0900

    fix zededitor command wrapper

--- zededitor/package.nix
+++ zededitor/package.nix
@@ -1,2 +1,18 @@
-{ zed-editor }:
-zed-editor
+{
+  lib,
+  makeBinaryWrapper,
+  symlinkJoin,
+  zed-editor,
+}:
+symlinkJoin {
+  name = "zededitor-${zed-editor.version}";
+  paths = [ zed-editor ];
+  nativeBuildInputs = [ makeBinaryWrapper ];
+  postBuild = ''
+    makeWrapper ${lib.getExe zed-editor} $out/bin/zededitor
+  '';
+
+  meta = zed-editor.meta // {
+    mainProgram = "zededitor";
+  };
+}
