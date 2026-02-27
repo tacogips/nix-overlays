225a643cb3fdbf0b28c5fba1857bee2a71adc35e
tacogips <me@tacogips.me>
2026-02-27 15:25:30 +0900

    Revert "fix zededitor command wrapper"
    This reverts commit 58a79c6cf993a87b94c7e42f34d15826462fe029.

--- zededitor/package.nix
+++ zededitor/package.nix
@@ -1,18 +1,2 @@
-{
-  lib,
-  makeBinaryWrapper,
-  symlinkJoin,
-  zed-editor,
-}:
-symlinkJoin {
-  name = "zededitor-${zed-editor.version}";
-  paths = [ zed-editor ];
-  nativeBuildInputs = [ makeBinaryWrapper ];
-  postBuild = ''
-    makeWrapper ${lib.getExe zed-editor} $out/bin/zededitor
-  '';
-
-  meta = zed-editor.meta // {
-    mainProgram = "zededitor";
-  };
-}
+{ zed-editor }:
+zed-editor
